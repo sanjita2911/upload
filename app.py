3f6e0b69cc7041e9c668f35f8395e539949d4065
sanjita2911 <nayaksanjita29@gmail.com>
2021-06-03 18:34:46 +0530

    jai

--- app.py
+++ app.py
@@ -1,31 +1,58 @@
-from flask import Flask, render_template, request,redirect
+from flask import Flask, render_template, request,redirect, url_for, session
 from flask_mysqldb import MySQL
-from flask_mail import Mail,Message
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Sinchana@00'
+app.config['MYSQL_DB'] = 'MyDB'
 
-app.config['MYSQL_HOST']='localhost'
+mysql = MySQL(app)
 
-app.config['MYSQL_USER']='root'
 
-app.config['MYSQL_PASSWORD']='SANJITA29112000'
 
-app.config['MYSQL_DB']='project'
-
-mysql= MySQL(app)
 mail=Mail(app)
+
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'nayaksanjita29@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Ranjita@29112000'
+app.config['MAIL_USERNAME'] = 'nie.clubhub@gmail.com'
+app.config['MAIL_PASSWORD'] = 'nie@mysore'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 
+
 @app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        details = request.form
+        name = details['name']
+        email = details['email']
+        subject = details['subject']
+        message=details['message']
+        
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO MyUser(name,email, subject, message) VALUES (%s, %s, %s, %s)",
+         (name,email, subject, message))
+        mysql.connection.commit()
+        cur.close()
+
+        msg = Message(subject, sender = 'nie.clubhub@gmail.com', recipients = [email])
+        msg.body ="Dear "+ name+ "!! You will here back from us soon !!!"
+        mail.send(msg)
+        return render_template('submit.html')
+    return render_template('index.html')
+
+
+@app.route('/clubs', methods=['GET', 'POST'])
+def clubs():
+    return render_template("clubs.html")
+
+@app.route('/member', methods=['GET', 'POST'])
 def member():
    if request.method == "POST":
         details = request.form
@@ -39,7 +66,7 @@ def member():
         semester=details['semester']
         clubs=details['clubs']
         di={
-            "IEEE":"sanjitavn29@gmail.com",
+            "IEEE":"sinchanapgowdakp@gmail.com",
             "Onyx":"sanjitavn29@gmail.com",
             "ISSA":"sanjitavn29@gmail.com",
             "UCSP":"sanjitavn29@gmail.com",
@@ -55,15 +82,17 @@ def member():
         mysql.connection.commit()
         cur.close()
 
-        msg = Message("Club Registration", sender = 'nayaksanjita29@gmail.com', recipients = [email])
+        msg = Message("Club Registration", sender = 'nie.clubhub@gmail.com', recipients = [email])
         msg.body ="Dear "+ firstname+ " !! You will here back from "+clubs+ " soon !"
         mail.send(msg)
-        msg1= Message("Registration request",sender ='nayaksanjita29@gmail.com',recipients= [di[clubs]])
-        msg1.body= "Name: " +firstname+" "+lastname+"\n"+"USN: "+usn+"\n"+"Gender: "+gender+"\n"+"Phone Number: "+phone+"\n"+"Branch: "+branch+"\n"+"Semester: "+semester+"\n"
+        msg1= Message("Registration request",sender ='nie.clubhub@gmail.com',recipients= [di[clubs]])
+        msg1.body= "Dear core member of "+clubs+"!! You have a membership request from a student. The student details are given below :\n Name: " +firstname+" "+lastname+"\n"+"USN: "+usn+"\n"+"Gender: "+gender+"\n"+"Phone Number: "+phone+"\n"+"Branch: "+branch+"\n"+"Semester: "+semester+"\n"
         mail.send(msg1)
         return render_template('submit.html')
-   return render_template('fmem.html')
+   return render_template('member.html')
     
 
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug="true")
